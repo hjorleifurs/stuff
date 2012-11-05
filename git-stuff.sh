8364162d01e5f840aa446ea5681afaa1ef656470
Hjörleifur Sveinbjörnsson <hjorleifur@gmail.com>
2012-11-05 12:41:56 +0000

    some changes made

--- git-stuff.sh
+++ git-stuff.sh
@@ -6,7 +6,8 @@ echo "a) fetch RHI.git (clone)"
 
 echo "b) fetch stuff.git (clone)"
 echo "c) add and commit changes"
-echo "p) push changes back to git"
+echo "1) push changes back to RHI.git"
+echo "2) push changes back to stuff.git"
 echo
 echo "x) Exit"
 
@@ -26,9 +27,13 @@ case $val in
 	   git add $1
 	   git commit -a
 	   ;;
-	p|P) 
-	   echo "push your stuff back to github"
+	1) 
+	   echo "push your stuff back to github RHI.git"
 	   git push -u http://github.com/hjorleifurs/RHI.git
+	   ;;
+	2) 
+	   echo "push your stuff back to github stuff.git"
+	   git push -u http://github.com/hjorleifurs/stuff.git
 	   ;;
 	x|X) exit ;;
 esac
