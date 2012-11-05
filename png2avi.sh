73e95ac25f4a6a24518ac566762e3145f559bf62
Hjörleifur Sveinbjörnsson <hjorleifur@gmail.com>
2012-11-05 14:04:41 +0000

    Update png2avi.sh
    Script that combines png files and the output is an avi file

--- png2avi.sh
+++ png2avi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 #mencoder mf://*.png -mf fps=$1 -ovc copy -o output.avi
+#usage: png2avi.sh fps name, ie. png2avi.sh 25 great-name
+#output would be the file great-name.avi and it would be 25 frames per second
 
 mencoder "mf://*.png" -ovc x264 -fps $1 -o $2.avi
