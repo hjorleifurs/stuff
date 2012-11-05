099395ee757dfa4988e83dd1e7ac9ed7d4ea876b
Hjörleifur Sveinbjörnsson <hjorleifur@gmail.com>
2012-11-05 12:36:11 +0000

    Add everything

---
+++ git-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo 
+echo
+
+echo "some useful git commands"
+
+echo
+echo "git clone https://github.com/hjorleifurs/RHI.git"
+echo
+echo "cd RHI"
+echo "change some files"
+echo "like add and directory with some files in it"
+echo "mkdir somedir"
+echo "echo jojojo > somedir/junk.txt"
+echo
+echo git add somedir/junk.txt
+echo
+echo git commit -a
+echo write some comment
+echo
+echo push it back to the repository
+echo git push -u https://github.com/hjorleifurs/RHI.git
